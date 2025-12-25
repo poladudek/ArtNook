408acc0c88084701d20173bdec9c28119202068f
poladudek <dudekpola24@gmail.com>
2025-12-25 03:41:54 +0100

    Add base createScreening funcs

--- models/screening.js
+++ models/screening.js
@@ -4,8 +4,14 @@ ScreeningModel = {
        const [all_screenings] = await db.query('SELECT * from Screenings');
         return all_screenings;
     },
-    createScreening: async ()  => {
-        return;
+    createScreening: async (movie_id, room_id, start_time) => {
+        const [duration] = await db.query('SELECT duration FROM Movies WHERE id = ?', [movie_id]);
+        const duration_in_min = duration[0].duration;
+        const startDate = new Date(start_time);
+        const endDate = new Date(startDate.getTime() + duration_in_min * 60000);
+
+        await db.query( `INSERT INTO Screenings (movie_id, room_id, start_time, end_time)
+        VALUES (?, ?, ?, ?)`, [movie_id, room_id, startDate, endDate]);
     },
     deleteScreening: async (id) => {
         await db.query(`DELETE FROM Screenings WHERE id= ?`, [id]);
--- services/screening.js
+++ services/screening.js
@@ -6,6 +6,16 @@ const ScreeningService = {
   },
 
   createScreening: async (data) => {
+    const {movie_id, room_id, start_time} = data;
+    //const is_movie = movieModel.isMovie(movie_id);
+    //if (is_movie === false){
+      //throw new Error('Movie is not in table');
+    //}
+    //const is_room_used = screeningModel.isRoomUsed(room_id, start_id);
+    //if (is_room_used === true){
+      //throw new Error('Room is already in use');
+    //}
+    screeningModel.createScreening(movie_id, room_id, start_time);
 
   },
 
